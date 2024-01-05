5c0ba65c655221c703d896695104196de8148570
Błażej Janus <82719790+blazejjanus@users.noreply.github.com>
2024-01-05 11:56:47 +0100

    v1.0

---
+++ Crypt/Program.cs
@@ -0,0 +1,118 @@
+﻿using CommandLine;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Crypt {
+    internal class Program {
+
+        public class Options {
+            [Option('e', "encrypt", HelpText = "Encrypt the file")]
+            public bool Encrypt { get; set; }
+
+            [Option('d', "decrypt", HelpText = "Decrypt the file")]
+            public bool Decrypt { get; set; }
+
+            [Value(0, MetaName = "file_path", HelpText = "The path to the file to encrypt or decrypt")]
+            public string FilePath { get; set; }
+
+            [Option('s', "symmetric", HelpText = "Use symmetric encryption")]
+            public bool Symmetric { get; set; }
+
+            [Option('a', "asymmetric", HelpText = "Use asymmetric encryption")]
+            public bool Asymmetric { get; set; }
+
+            [Option("key", HelpText = "The key for symmetric encryption")]
+            public string Key { get; set; }
+
+            [Option("public_key", HelpText = "The path to the public key for asymmetric encryption")]
+            public string PublicKeyPath { get; set; }
+
+            [Option("private_key", HelpText = "The path to the private key for asymmetric encryption")]
+            public string PrivateKeyPath { get; set; }
+        }
+
+        static void Main(string[] args) {
+            try {
+                Parser.Default.ParseArguments<Options>(args).WithParsed(options => RunOptions(options));
+            } catch(Exception exc) {
+                Console.WriteLine(exc.Message);
+            }
+        }
+
+        private static void RunOptions(Options options) {
+            if(options.Encrypt && options.Symmetric) {
+                EncryptWithSymmetricKey(options.FilePath, options.Key);
+            } else if(options.Encrypt && options.Asymmetric) {
+                EncryptWithAsymmetricKey(options.FilePath, options.PublicKeyPath);
+            } else if(options.Decrypt && options.Symmetric) {
+                DecryptWithSymmetricKey(options.FilePath, options.Key);
+            } else if(options.Decrypt && options.Asymmetric) {
+                DecryptWithAsymmetricKey(options.FilePath, options.PrivateKeyPath);
+            } else {
+                Console.WriteLine("Invalid options provided.");
+            }
+        }
+
+        #region AES
+        static void EncryptWithSymmetricKey(string filePath, string password) {
+            string plainText = File.ReadAllText(filePath);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            using(AesManaged aes = new AesManaged()) {
+                aes.GenerateIV();
+                aes.GenerateKey();
+                using(RijndaelManaged rijndael = new RijndaelManaged()) {
+                    rijndael.Mode = CipherMode.ECB;
+                    rijndael.Padding = PaddingMode.PKCS7;
+                    rijndael.BlockSize = 128;
+                    using(ICryptoTransform encryptor = rijndael.CreateEncryptor(aes.Key, aes.IV)) {
+                        byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+                        byte[] encryptedBytes = encryptor.TransformFinalBlock(plainTextBytes, 0, plainTextBytes.Length);
+                        File.WriteAllBytes(filePath + ".crypt", encryptedBytes);
+                    }
+                }
+            }
+        }
+
+        static void DecryptWithSymmetricKey(string filePath, string password) {
+            byte[] cipherText = File.ReadAllBytes(filePath);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            using(AesManaged aes = new AesManaged()) {
+                aes.GenerateIV();
+                aes.GenerateKey();
+                using(RijndaelManaged rijndael = new RijndaelManaged()) {
+                    rijndael.Mode = CipherMode.ECB;
+                    rijndael.Padding = PaddingMode.PKCS7;
+                    rijndael.BlockSize = 128;
+                    using(ICryptoTransform decryptor = rijndael.CreateDecryptor(aes.Key, aes.IV)) {
+                        byte[] decryptedBytes = decryptor.TransformFinalBlock(cipherText, 0, cipherText.Length);
+                        string decrypted = Encoding.UTF8.GetString(decryptedBytes);
+                        File.WriteAllText(filePath.Replace(".crypt", ""), decrypted);
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region RSA
+        private static void EncryptWithAsymmetricKey(string filePath, string publicKeyPath) {
+            string plainText = File.ReadAllText(filePath);
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            using(var rsa = new RSACryptoServiceProvider()) {
+                rsa.FromXmlString(File.ReadAllText(publicKeyPath));
+                byte[] encryptedBytes = rsa.Encrypt(plainTextBytes, false);
+                File.WriteAllBytes(filePath + ".crypt", encryptedBytes);
+            }
+        }
+
+        private static void DecryptWithAsymmetricKey(string filePath, string privateKeyPath) {
+            byte[] cipherText = File.ReadAllBytes(filePath);
+            using(var rsa = new RSACryptoServiceProvider()) {
+                rsa.FromXmlString(File.ReadAllText(privateKeyPath));
+                byte[] decryptedBytes = rsa.Decrypt(cipherText, false);
+                string decrypted = Encoding.UTF8.GetString(decryptedBytes);
+                File.WriteAllText(filePath.Replace(".crypt", ""), decrypted);
+            }
+        }
+        #endregion
+    }
+}
---
+++ Hash/Program.cs
@@ -0,0 +1,122 @@
+﻿using CommandLine;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Hash {
+    internal class Program { 
+
+        public class Options {
+            [Option('c', "calculate", SetName = "action", HelpText = "Calculate hash for a file.")]
+            public bool Calculate { get; set; }
+
+            [Option('v', "verify", SetName = "action", HelpText = "Verify hash for a file.")]
+            public bool Verify { get; set; }
+
+            [Option('h', "hash", HelpText = "Specify hash function.")]
+            public string HashFunction { get; set; } = "sha256";
+
+            [Value(0, MetaName = "file path", HelpText = "Path to the file.")]
+            public string FilePath { get; set; }
+
+            [Value(1, MetaName = "hash", HelpText = "Hash to verify against.")]
+            public string Hash { get; set; }
+        }
+
+        static void Main(string[] args) {
+            try {
+                Parser.Default.ParseArguments<Options>(args).WithParsed(options => RunOptions(options));
+            } catch (Exception exc) {
+                Console.WriteLine(exc.Message);
+            }
+        }
+
+        private static void RunOptions(Options options) {
+            if(options.Calculate) {
+                DisplayFileHash(options.FilePath, options.HashFunction);
+            } else if(options.Verify) {
+                VerifyFileHash(options.FilePath, options.Hash, options.HashFunction);
+            } else {
+                Console.WriteLine("Please specify either -c or -v");
+            }
+        }
+
+        private static void DisplayFileHash(string filePath, string hashFunction = "sha256") {
+            var hash = CalculateFileHash(filePath, hashFunction);
+            Console.WriteLine($"{hashFunction}: {hash}");
+        }
+
+        private static void VerifyFileHash(string filePath, string expectedHash, string hashFunction = "sha256") {
+            expectedHash = expectedHash.Trim();
+            if(!IsValidHash(expectedHash, hashFunction)) {
+                throw new ArgumentException("Provided hash is not in valid format!");
+            }
+            var hash = CalculateFileHash(filePath, hashFunction);
+            Console.WriteLine($"Provided hash:   {hashFunction}: {expectedHash}");
+            Console.WriteLine($"Calculated hash: {hashFunction}: {hash}");
+            if(CompareHashes(expectedHash, hash)) {
+                Console.WriteLine("OK, provided hashes are the same.");
+            } else {
+                Console.WriteLine("ERROR, provided hashes differ.");
+            }
+        }
+
+        private static string CalculateFileHash(string filePath, string hashFunction = "sha256") {
+            string input = File.ReadAllText(filePath);
+            hashFunction = hashFunction.Trim();
+            hashFunction = hashFunction.ToLower();
+            return hashFunction switch {
+                "sha256" => CalcSha256(input),
+                "md5" => CalcMd5(input),
+                _ => throw new ArgumentException("Only sha256 and md5 are supported!", nameof(hashFunction)),
+            };
+        }
+
+        private static string CalcMd5(string input) {
+            using(MD5 md5 = MD5.Create()) {
+                byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+                StringBuilder sb = new StringBuilder();
+                for(int i = 0; i < hashBytes.Length; i++) {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+
+        private static string CalcSha256(string input) {
+            using(SHA256 sha256Hash = SHA256.Create()) {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+                StringBuilder builder = new StringBuilder();
+                for(int i = 0; i < bytes.Length; i++) {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        private static bool IsValidHash(string hash, string hashFunction = "sha256") {
+            hashFunction = hashFunction.Trim();
+            hashFunction = hashFunction.ToLower();
+            return hashFunction switch {
+                "sha256" => IsValidSha256(hash),
+                "md5" => IsValidMd5(hash),
+                _ => throw new ArgumentException("Only sha256 and md5 are supported!", nameof(hashFunction)),
+            };
+        }
+
+        private static bool IsValidMd5(string hash) {
+            Regex regex = new Regex(@"^[a-fA-F0-9]{32}$");
+            return regex.IsMatch(hash);
+        }
+
+        private static bool IsValidSha256(string hash) {
+            Regex regex = new Regex(@"^[a-fA-F0-9]{64}$");
+            return regex.IsMatch(hash);
+        }
+
+        private static bool CompareHashes(string hash1, string hash2) {
+            return hash1.Equals(hash2, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
