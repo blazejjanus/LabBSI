0697f8e8ca87c09640b84cd69f9d6076a4820a0a
Błażej Janus <82719790+blazejjanus@users.noreply.github.com>
2024-01-08 13:45:25 +0100

    v0.1

---
+++ CodingComparison/Result.cs
@@ -0,0 +1,11 @@
+﻿namespace CodingComparison {
+    internal class Result {
+        public string File {  get; set; }
+        public string Algorithm { get; set; }
+        public TimeSpan ElapsedTime { get; set; }
+        public int MemoryUsed { get; set; }
+        public int InputSize { get; set; }
+        public int OutputSize { get; set; }
+        public double CompressionRatio => InputSize / OutputSize;
+    }
+}
